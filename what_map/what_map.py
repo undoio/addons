8ba9f8608e151a1874d80a6c5e6ef79ad091f68f
Toby Lloyd Davies <tlloyddavies@undo.io>
2019-08-21 17:53:40 +0100

    fix style

--- what_map/what_map.py
+++ what_map/what_map.py
@@ -15,8 +15,8 @@ import gdb
 # Pattern to parse map.
 begin_pattern = re.compile(
     r'(?P<begin>[0-9a-fA-F]+)-(?P<end>[0-9a-fA-F]+)'
-    r'\s+([rwxps-]+)\s+([0-9a-fA-F]+)\s+([0-9a-fA-F]+):([0-9a-fA-F]+)\s+([0-9]+)[ ]*'
-    r'([^\n]*)')
+    r'\s+([rwxps-]+)\s+([0-9a-fA-F]+)\s+([0-9a-fA-F]+):([0-9a-fA-F]+)\s+'
+    r'([0-9]+)[ ]*([^\n]*)')
 
 
 def fetch_maps_remote():
