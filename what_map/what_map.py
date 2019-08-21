f95f911ef40c14eb7609ad36511fcc1d0723c98c
Toby Lloyd Davies <tlloyddavies@undo.io>
2019-08-21 17:27:07 +0100

    fix multiline regular expression

--- what_map/what_map.py
+++ what_map/what_map.py
@@ -13,9 +13,10 @@ import re
 import gdb
 
 # Pattern to parse map.
-begin_pattern = re.compile(r'''(?P<begin>[0-9a-fA-F]+)-(?P<end>[0-9a-fA-F]+)
-\s+([rwxps-]+)\s+([0-9a-fA-F]+)\s+([0-9a-fA-F]+):([0-9a-fA-F]+)\s+([0-9]+)[ ]*
-([^\n]*)''')
+begin_pattern = re.compile(
+    r'(?P<begin>[0-9a-fA-F]+)-(?P<end>[0-9a-fA-F]+)'
+    r'\s+([rwxps-]+)\s+([0-9a-fA-F]+)\s+([0-9a-fA-F]+):([0-9a-fA-F]+)\s+([0-9]+)[ ]*'
+    r'([^\n]*)')
 
 
 def fetch_maps_remote():
