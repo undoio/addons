dfb1224d45cad83bfa8f71e7bb264a3287004990
Toby Lloyd Davies <tlloyddavies@undo.io>
2019-08-22 15:30:42 +0100

    initial commit

---
+++ count_calls/README.md
@@ -0,0 +1,7 @@
+Count calls
+===========
+
+Counts how many times a function was called in a recording.
+
+	usage:
+	./count_calls RECORDING_FILE FUNCTION_NAME
---
+++ count_calls/count_calls.py
@@ -0,0 +1,56 @@
+#! /usr/bin/env udb-automate
+
+from __future__ import absolute_import, division, print_function
+
+import sys
+import textwrap
+
+from undodb.udb_launcher import UdbLauncher
+
+
+def main(argv):
+    # Get the arguments.
+    try:
+        recording, func_name = argv[1:]
+    except ValueError:
+        print('{} RECORDING_FILE FUNCTION_NAME'.format(sys.argv[0]))
+        raise SystemExit(1)
+
+    # Prepare for launching UndoDB.
+    launcher = UdbLauncher()
+    # Make UndoDB run with our recording.
+    launcher.recording_file = recording
+    # Make UndoDB load the extension.py file from the current directory.
+    launcher.add_extension('extension')
+    # Tell the extension the function name.
+    launcher.run_data['func_name'] = func_name
+    # Finally, launch UndoDB! (And hide the output, we don't want it on
+    # screen.)
+    res = launcher.run_debugger(redirect_output=True)
+
+    if res.exit_code == 0:
+        # All good.
+        print('The recording hit "{}" {} time(s).'.format(
+            func_name,
+            res.result_data['hit-count'],
+            ))
+    else:
+        # Something went wrong! Print a useful message.
+        print(textwrap.dedent(
+            '''\
+            Error!
+            UndoDB exited with code {res.exit_code}.
+
+            The output is:
+            {div}
+            {res.output}
+            {div}\
+            ''').format(
+                res=res,
+                div='-' * 72,
+                ))
+        raise SystemExit(res.exit_code)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
